4019016a648c83c2abbaa8eeaec1c1c9deba7be2
zacrichmond <zacrichmond@users.noreply.github.com>
2021-04-04 02:24:03 +1000

    added firebase auth and task creation

--- bananafrontend/src/App.js
+++ bananafrontend/src/App.js
@@ -1,22 +1,96 @@
-import React, { useState, Component, Children } from "react";
-
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-import Button from "@material-ui/core/Button";
+import React from "react";
 
 import { MainPage } from "./main-page";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import "./App.css";
 
-function App() {
-  const [userIsAuth, setUserIsAuth] = useState(false);
-  const [showText, setShowText] = useState(false);
+const firebaseConfig = {
+  apiKey: "AIzaSyBaznSoNj7LuOZnaVTtxcj9r896M_4eEY0",
+  authDomain: "bananaboats-7a42b.firebaseapp.com",
+  projectId: "bananaboats-7a42b",
+  storageBucket: "bananaboats-7a42b.appspot.com",
+  messagingSenderId: "613212672185",
+  appId: "1:613212672185:web:34dc7058fc37fdb3040b68",
+  measurementId: "G-4Q4K5JSHE0",
+};
+
+// Initialize Firebase if not exists
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const auth = firebase.auth();
+
+function SignInButton() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
 
   return (
+    <Button variant="contained" color="primary" onClick={signInWithGoogle}>
+      Sign in with Google
+    </Button>
+  );
+}
+
+function SignOutButton() {
+  return (
+    <Button variant="contained" color="primary" onClick={() => auth.signOut()}>
+      Sign Out
+    </Button>
+  );
+}
+
+function App() {
+  const [user, loading] = useAuthState(auth);
+
+  // Loading
+  if (loading) {
+    return (
+      <div className="App">
+        <CssBaseline />
+        <div className="LoadingScreen">
+          <CircularProgress />
+        </div>
+      </div>
+    );
+  }
+
+  // User is logged in
+  if (user) {
+    return (
+      <div className="App">
+        <CssBaseline />
+        <div>
+          <p>UId: {user.uid}</p>
+        </div>
+
+        <SignOutButton />
+        <hr />
+        <MainPage firebase={firebase} />
+      </div>
+    );
+  }
+
+  // User is not logged in
+  return (
     <div className="App">
-      if the user is logged in show the main page else if the use is not logged
-      in show the login page
-      <MainPage />
+      <CssBaseline />
+      <SignInButton />
+      <p>This is where the Landing page will go :)</p>
     </div>
   );
 }
--- bananafrontend/src/main-page/main.jsx
+++ bananafrontend/src/main-page/main.jsx
@@ -2,18 +2,109 @@ import React, { useState } from "react";
 
 import { TaskPanel } from "./task-panel";
 
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 
 export function MainPage(props) {
+  const { firebase } = props;
+
+  const auth = firebase.auth();
+  const firestore = firebase.firestore();
+
   const [taskPanelOpen, setTaskPanelOpen] = useState(true);
+
+  const user = auth.currentUser;
+  const timeStamp = new Date("2021-04-01");
+
+  // Load all valid tasks belonging to User
+  const tasksRef = firestore.collection("tasks");
+  const query = tasksRef
+    .where("uid", "==", user.uid)
+    .where("scheduled", ">=", timeStamp);
+  const [tasks, loading] = useCollectionData(query, { idField: "id" });
+
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [newTaskLength, setNewTaskLength] = useState(1);
+
+  // Create a new task
+  const createTask = async (e) => {
+    e.preventDefault();
+
+    const undefinedDate = new Date("2100-01-01");
+    const uid = user.uid;
+
+    await tasksRef.add({
+      created: firebase.firestore.FieldValue.serverTimestamp(),
+      scheduled: undefinedDate,
+      title: newTaskTitle,
+      length: newTaskLength,
+      uid,
+    });
+
+    setNewTaskTitle("");
+    setNewTaskLength(1);
+  };
+
+  // Turn the tasks into components to show on screen
+  const taskCards =
+    !loading && tasks
+      ? tasks.map((task) => {
+          return (
+            <Card>
+              <CardContent>
+                <Typography>Task: {task.title}</Typography>
+                <Typography>Length: {task.length}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })
+      : [<p>No tasks</p>];
 
   return (
     <div>
-      <CssBaseline />
       <TaskPanel isOpen={taskPanelOpen} setIsOpen={setTaskPanelOpen} />
-      <Button onClick={() => setTaskPanelOpen(!taskPanelOpen)}>
-        Open close
+
+      <div>{taskCards}</div>
+
+      <form onSubmit={createTask}>
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          required
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Length"
+          variant="outlined"
+          required
+          type="number"
+          step="0.5"
+          endAdornment={<InputAdornment position="end">Hr</InputAdornment>}
+          onChange={(e) => setNewTaskLength(e.target.value)}
+        />
+
+        <Button variant="contained" color="primary" type="submit">
+          Create new task
+        </Button>
+      </form>
+
+      <hr />
+
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => setTaskPanelOpen(!taskPanelOpen)}
+      >
+        Open/Close Side Panel
       </Button>
     </div>
   );
